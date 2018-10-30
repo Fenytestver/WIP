82e756e34c967c804d47caa06e34c8c419eea981
Marcell <marcellkovacsus@gmail.com>
2018-10-29 22:57:00 -0700

    display fixin, still memory leak somewhere.

--- SumpPitSensorProject/test/testmain.cpp
+++ SumpPitSensorProject/test/testmain.cpp
@@ -30,18 +30,18 @@ void TestMain::run()
 }
 void TestMain::runFunctionalTests()
 {
-  start(new test_normaloperation());
-  start(new test_pump_failure_functional());
+  //start(new test_normaloperation());
+  //start(new test_pump_failure_functional());
   // LCD display samples
   start(new test_display_demo());
 }
 
 void TestMain::runSmallTests()
 {
-  start(new test_pump_unit());
+  /*start(new test_pump_unit());
   start(new test_multipump_unit());
   start(new test_multipump_unit());
-  start(new test_buttons());
+  start(new test_buttons());*/
 }
 
 void TestMain::start(TestBase* testCase)
