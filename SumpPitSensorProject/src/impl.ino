47f228973514d547aa238c32f0e5bdd9fdcc172c
Marcell <marcellkovacsus@gmail.com>
2018-12-29 19:56:00 -0800

    revert config to photon

--- SumpPitSensorProject/src/impl.ino
+++ SumpPitSensorProject/src/impl.ino
@@ -11,7 +11,7 @@
 #include "application.h"
 #include "rpmsensor.h"
 #include "display.h"
-#include "LiquidCrystal_I2C_Spark.h"
+#include "LiquidCrystal_I2C.h"
 #include "state.h";
 
 #define PUB_SHUTOFF_STATE "shutoff-valve-state"
