82159aa05b3e5332df62f5181c0c3c6916ada0a0
Marcell <marcellkovacsus@gmail.com>
2019-01-07 19:42:40 -0800

    More precise square wave calculation.

--- squarewave/squarewave.ino
+++ squarewave/squarewave.ino
@@ -3,8 +3,8 @@
 #define PIN_OUT_2 3
 
 // enter RPM here
-#define RPM1 300
-#define RPM2 400
+#define RPM1 500
+#define RPM2 800
 
 long p1half = 0;
 long p2half = 0;
@@ -16,18 +16,18 @@ bool p2 = false;
 void setup() {
   pinMode(PIN_OUT_1, OUTPUT);
   pinMode(PIN_OUT_2, OUTPUT);
-  p1half = 1000000L / RPM1 / 2;
-  p2half = 1000000L / RPM2 / 2;
+  p1half = round(1000000.0 / ((double)RPM1) / 2.0);
+  p2half = round(1000000.0 / ((double)RPM2) / 2.0);
 }
 
 void loop() {
   long now = micros();
-  if (now - p1time >= p1half) {
+  if (now - p1time > p1half) {
     p1 = !p1;
     digitalWrite(PIN_OUT_1, p1 ? LOW : HIGH);
     p1time = now;
   }
-  if (now - p2time >= p2half) {
+  if (now - p2time > p2half) {
     p2 = !p2;
     digitalWrite(PIN_OUT_2, p2 ? LOW : HIGH);
     p2time = now;
