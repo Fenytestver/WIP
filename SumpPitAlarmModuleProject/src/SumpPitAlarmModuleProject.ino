44f2f661e33193bedc13f78684ec9956335f6e93
Marcell <marcellkovacsus@gmail.com>
2019-01-27 12:19:12 -0800

    Add float switch to shutoff alerts. Increment model version to 0.0.3. Depend on it. Parse error code, only start siren for shutoff level alerts (CriticalB). Implement alarm lights with some extras.

--- SumpPitAlarmModuleProject/src/SumpPitAlarmModuleProject.ino
+++ SumpPitAlarmModuleProject/src/SumpPitAlarmModuleProject.ino
@@ -43,9 +43,12 @@ RealSiren* siren;
 bool technical = false;
 bool critical = false;
 int mode = SPN_MODE_UNKNOWN;
+long alarmReason = SPN_ALARM_NO_ALARM;
 char* dataCpy = new char[1024];
 bool stateUnknown = true;
 unsigned long snoozeAt = 0L;
+unsigned long lastCritical = 0L;
+bool sirenOn = false;
 
 class OnAnyPress : public OnButtonPressListener {
       public:
@@ -95,25 +98,44 @@ void loop() {
   long nowbit = now / 200;
   switch (mode) {
     case SPN_MODE_UNKNOWN:
-      ledRed->setState((nowbit % 3) == 0);
-      ledRed2->setState((nowbit % 3) == 0);
-      ledRed3->setState((nowbit % 3) == 0);
-      ledGreen->setState((nowbit % 3) == 1);
-      ledYellow->setState((nowbit % 3) == 2);
+      ledRed->setState((nowbit % 5) == 2);
+      ledRed2->setState((nowbit % 5) == 3);
+      ledRed3->setState((nowbit % 5) == 4);
+      ledGreen->setState((nowbit % 5) == 0);
+      ledYellow->setState((nowbit % 5) == 1);
+      sirenOn = false;
       break;
-    case SPN_ARMED:
-      ledRed->setState(critical);
-      ledRed2->setState(critical);
-      ledRed3->setState(critical);
-      ledGreen->setState(true);
-      ledYellow->setState(technical);
+    case SPN_ARMED: {
+        if (critical && lastCritical == 0L) {
+          lastCritical = now;
+        } else if (critical && lastCritical == 0L) {
+          lastCritical = 0L;
+        }
+        long criticalDuration = lastCritical > 0 ?
+            now - lastCritical
+            : 0L;
+        if (isCriticalShutoff(alarmReason)) {
+          renderCriticalLeds(criticalDuration, true /* shutoff */, nowbit);
+        } else if (critical) {
+          renderCriticalLeds(criticalDuration, false /* shutoff */, nowbit);
+        } else {
+          ledRed->setState(false);
+          ledRed2->setState(false);
+          ledRed3->setState(false);
+          sirenOn = false;
+        }
+        ledGreen->setState(true);
+        ledYellow->setState(technical && ((nowbit % 2) == 0));
+      }
       break;
-    case SPN_DISARMED:
-      ledRed->setState(true);
-      ledRed2->setState(true);
-      ledRed3->setState(true);
-      ledGreen->setState(false);
-      ledYellow->setState(true);
+    case SPN_DISARMED: {
+        ledRed->setState(true);
+        ledRed2->setState(true);
+        ledRed3->setState(true);
+        ledGreen->setState(false);
+        ledYellow->setState(true);
+        sirenOn = false;
+      }
       break;
     case SPN_MAINTENANCE:
       ledRed->setState(false);
@@ -121,10 +143,11 @@ void loop() {
       ledRed3->setState(false);
       ledGreen->setState(false);
       ledYellow->setState(true);
+      sirenOn = false;
       break;
   }
 
-  if (critical && (snoozeAt == 0 || snoozeAt + SPN_SNOOZE_TIME < now)) {
+  if (sirenOn && (snoozeAt == 0 || snoozeAt + SPN_SNOOZE_TIME < now)) {
     siren->on();
   } else {
     siren->off();
@@ -166,17 +189,22 @@ void statusHandler(const char* event, const char* data) {
   Serial.print(data);
   Serial.println();
   strcpy(dataCpy, data);
-  Serial.print(dataCpy);
-  Serial.println();
 
   Serial.print("Mode ");
   bool isValid = false;
   jsmnrtype_t objType = JSMNR_UNDEFINED;
   int objSize = 0;
-
   String mStr = RdJson::getString("mode", "", isValid, objType, objSize, dataCpy);
   Serial.println(mStr);
   mode = stringToInt(mStr, SPN_MODE_UNKNOWN);
+
+  String alarmStr =
+      RdJson::getString("alarm", "", isValid, objType, objSize, dataCpy);
+  alarmReason = binaryStringToInt(alarmStr, SPN_ALARM_NO_ALARM);
+  Serial.print("alarmStr:");
+  Serial.print(alarmStr);
+  Serial.print("-");
+  Serial.println(alarmReason);
 
   technical = RdJson::getString("technical", "", isValid, objType, objSize, dataCpy) == "1";
   critical = RdJson::getString("critical", "", isValid, objType, objSize, dataCpy) == "1";
@@ -184,6 +212,27 @@ void statusHandler(const char* event, const char* data) {
   Serial.println(technical);
   Serial.print("critical");
   Serial.println(critical);
+}
+
+long binaryStringToInt(String extra, long defaultInt) {
+  // little endian
+  long result = defaultInt;
+  if (extra != nullptr) {
+    char c = extra[0];
+    int extraIndex = 0;
+    int bitpos = 0;
+    int extralen = extra.length();
+    while (c != '\n' && extraIndex < extralen) {
+      if (c == '0') {
+        bitpos++;
+      } else if (c == '1') {
+        result |= (1<<bitpos);
+        bitpos++;
+      }
+      c = extra[++extraIndex];
+    }
+  }
+  return result;
 }
 
 int stringToInt(String extra, int defaultInt) {
@@ -195,4 +244,52 @@ int stringToInt(String extra, int defaultInt) {
     }
   }
   return result;
+}
+
+bool isCriticalShutoff(long alarm) {
+  return ((SPN_ALERT_ALL_CRITICAL_SHUTOFF & alarm) != 0);
+}
+
+void renderCriticalLeds(long duration, bool shutoff, long nowBits) {
+  long sec = duration / 100;
+  if (shutoff) {
+    if (sec > 240) {
+      ledRed->setState((nowBits & 2) == 0);
+      ledRed2->setState((nowBits & 2) == 0);
+      ledRed3->setState((nowBits & 2) == 0);
+      sirenOn = true;
+    } else if (sec > 120) {
+      ledRed->setState(true);
+      ledRed2->setState(true);
+      ledRed3->setState((nowBits & 2) == 0);
+      sirenOn = false;
+    } else if (sec > 60) {
+      ledRed->setState(true);
+      ledRed2->setState((nowBits & 2) == 0);
+      ledRed3->setState(false);
+      sirenOn = false;
+    } else {
+      ledRed->setState((nowBits & 2) == 0);
+      ledRed2->setState(false);
+      ledRed3->setState(false);
+      sirenOn = false;
+    }
+  } else {
+    // alert A
+    sec = sec % 360;
+    sirenOn = false;
+    if (sec > 300) {
+      ledRed->setState(true);
+      ledRed2->setState(true);
+      ledRed3->setState(true);
+    } else if (sec > 120) {
+      ledRed->setState(true);
+      ledRed2->setState(true);
+      ledRed3->setState(false);
+    } else {
+      ledRed->setState(true);
+      ledRed2->setState(false);
+      ledRed3->setState(false);
+    }
+  }
 }
