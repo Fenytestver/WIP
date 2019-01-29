a02da5a2ca7e52c76a6c4dfecea27d40cc21fdaa
Marcell <marcellkovacsus@gmail.com>
2019-01-28 20:45:49 -0800

    Fix red led on disarmed on sensor.

--- sumppitmodel/src/localview.cpp
+++ sumppitmodel/src/localview.cpp
@@ -40,9 +40,10 @@ void LocalView::setup()
 
 void LocalView::render(State state)
 {
-  ledRed->setState(state.mode == Mode::SPN_DISARMED);
+  ledRed->setState(false);
   ledGreen->setState(state.mode == Mode::SPN_ARMED);
-  ledYellow->setState(state.mode == Mode::SPN_MAINTENANCE);
+  ledYellow->setState(state.mode == Mode::SPN_DISARMED
+      || state.mode == Mode::SPN_MAINTENANCE);
   int len;
   switch (state.mode) {
     case Mode::SPN_MAINTENANCE:
