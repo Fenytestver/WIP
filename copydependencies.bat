e8c628716d958d824b0c2f4763f473b59af99acd
Marcell <marcellkovacsus@gmail.com>
2019-01-29 19:19:47 -0800

    Add helper scripts to copy deps to place. And a cleanup script.

---
+++ copydependencies.bat
@@ -0,0 +1,7 @@
+mkdir SumpPitActuatorProject\lib
+mkdir SumpPitSensorProject\lib
+mkdir SumpPitAlarmModuleProject\lib
+
+cp -r sumppitmodel SumpPitActuatorProject/lib/
+cp -r sumppitmodel SumpPitSensorProject/lib/
+cp -r sumppitmodel SumpPitAlarmModuleProject/lib/
