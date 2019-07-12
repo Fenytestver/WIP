b58dca8e64c5eb8c7c811f4999b5f6becf43e7b5
Fenytestver <39752455+Fenytestver@users.noreply.github.com>
2019-07-11 20:30:55 -0400

    Add local setting example

--- SumpPitSensorProject/localsettings.h
+++ SumpPitSensorProject/localsettings.h
@@ -7,3 +7,61 @@
 //
 // #define PLATFORM PLATFORM_PHOTON
 // #define PLATFORM PLATFORM_ELECTRON
+
+/*Example for local settings, used for 00654
+
+// This file intended to contain local configuration.
+// Changes to this file should not be committed to the repo.
+
+// Example:
+// uncomment one of the lines below, keep the same lines commented in the
+// application.ino file.
+//
+#include "Particle.h"
+
+// #define PLATFORM PLATFORM_PHOTON
+#define PLATFORM PLATFORM_ELECTRON
+
+
+#define CLOUD_ENABLED true
+#define SYNC_ENABLED true
+#define SYNC_PERIOD_MIN 5000L
+#define SPN_BUTTON_LONG_PRESS_TIME 1000
+
+// PINs B0 to A7 (lh rail)
+
+#define PIN_LEAK_2 B0
+#define PIN_LEAK_1 B1
+#define PIN_PUMP_VOLTAGE_2 B2
+#define PIN_PUMP_VOLTAGE_1 B3
+#define PIN_PUMP_RPM_2 B4
+#define PIN_PUMP_RPM_1 B5
+// A0 - available
+// A1 - available
+#define PIN_SIREN A2
+// A3 - available DAC PIN NO 5V!
+// A4 - available
+#define PIN_WATERLEVEL A5
+// A6 - available DAC PIN NO 5V!
+// A7 - available WKP pin
+
+// PINs C0 to D7 (rh rail)
+
+#define PIN_BUTTON_3 C0
+#define PIN_BUTTON_2 C1
+#define PIN_BUTTON_1 C2
+// C3 - available
+// C4 - available Wire1 SDA!
+// C5 - available Wire1 SCL!
+// D0 > LCD SDA
+// D1 > LCD SCL
+// D2 - available
+#define PIN_LED_RED D3
+#define PIN_LED_YELLOW D4
+#define PIN_LED_GREEN D5
+#define PIN_FLOAT_SWITCH_1 D6
+#define PIN_BUZZER D7
+
+
+
+ */
